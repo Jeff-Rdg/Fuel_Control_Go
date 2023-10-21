bcf57200a6926cdbe850b55528822c9d53b42675
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2023-10-20 22:43:37 -0300

    feat methods to tank

--- entity/tank.go
+++ entity/tank.go
@@ -1,7 +1,39 @@
 package entity
 
+import (
+	"errors"
+	"github.com/google/uuid"
+	"time"
+)
+
+var (
+	TankCapacityError = errors.New("invalid tank capacity, value must be between 0 and 2000")
+)
+
 type Tank struct {
 	Base
 	capacity float64
 	quantity float64
+}
+
+func NewTank(capacity float64) (Tank, error) {
+	err := validateNewTank(capacity)
+	if err != nil {
+		return Tank{}, err
+	}
+	return Tank{
+		Base: Base{
+			id:        uuid.New(),
+			createdAt: time.Now(),
+			updatedAt: time.Now(),
+		},
+		capacity: capacity,
+	}, nil
+}
+
+func validateNewTank(capacity float64) error {
+	if capacity <= 0 || capacity > 2000 {
+		return TankCapacityError
+	}
+	return nil
 }
