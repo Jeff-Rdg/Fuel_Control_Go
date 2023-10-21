5b48812a51938104a44eee48c34efab3e9b5be34
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2023-10-20 23:23:49 -0300

    fix method validate

--- entity/owner.go
+++ entity/owner.go
@@ -26,6 +26,7 @@ func NewOwner(cnpj, coorporateName, email string) (Owner, error) {
 	if err != nil {
 		return Owner{}, err
 	}
+
 	return Owner{
 		Base: Base{
 			id:        uuid.New(),
@@ -58,29 +59,44 @@ func validateCnpj(cnpj string) bool {
 	if len(cnpj) != 14 {
 		return false
 	}
-
 	sum := 0
+	multiplier := 5
 	for i := 0; i < 12; i++ {
-		sum += int(cnpj[i]-'0') * (i + 2)
+		digit := int(cnpj[i] - '0')
+		sum += digit * multiplier
+		multiplier--
+		if multiplier < 2 {
+			multiplier = 9
+		}
 	}
-	remainder := sum % 11
-	digit1 := 11 - remainder
-	if digit1 >= 10 {
+	digit1 := sum % 11
+	if digit1 < 2 {
 		digit1 = 0
+	} else {
+		digit1 = 11 - digit1
 	}
+
 	if digit1 != int(cnpj[12]-'0') {
 		return false
 	}
 
 	sum = 0
+	multiplier = 6
 	for i := 0; i < 13; i++ {
-		sum += int(cnpj[i]-'0') * (i + 2)
+		digit := int(cnpj[i] - '0')
+		sum += digit * multiplier
+		multiplier--
+		if multiplier < 2 {
+			multiplier = 9
+		}
 	}
-	remainder = sum % 11
-	digit2 := 11 - remainder
-	if digit2 >= 10 {
+	digit2 := sum % 11
+	if digit2 < 2 {
 		digit2 = 0
+	} else {
+		digit2 = 11 - digit2
 	}
+
 	if digit2 != int(cnpj[13]-'0') {
 		return false
 	}
