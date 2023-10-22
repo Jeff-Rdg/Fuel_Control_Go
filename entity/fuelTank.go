5cb42b19161738a8b8b60292096342d5b7fc96fd
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2023-10-21 21:20:36 -0300

    impl factory to fuelTank.go

--- entity/fuelTank.go
+++ entity/fuelTank.go
@@ -1,9 +1,34 @@
 package entity
 
-import "github.com/google/uuid"
+import (
+	"errors"
+	"github.com/google/uuid"
+	"time"
+)
+
+var (
+	FuelTankQuantityError = errors.New("invalid quantity to supply")
+	FuelTankDateError     = errors.New("invalid date to supply")
+)
 
 type FuelTank struct {
-	FromFuel uuid.UUID
-	ToTank   uuid.UUID
-	Quantity float64
+	fromFuel   uuid.UUID
+	toTank     uuid.UUID
+	quantity   float64
+	supplyDate time.Time
+}
+
+func NewFuelTank(fromFuel, toTank uuid.UUID, quantity float64, date time.Time) (FuelTank, error) {
+	if quantity <= 0 {
+		return FuelTank{}, FuelTankQuantityError
+	}
+	if date.After(time.Now()) {
+		return FuelTank{}, FuelTankDateError
+	}
+	return FuelTank{
+		fromFuel:   fromFuel,
+		toTank:     toTank,
+		quantity:   quantity,
+		supplyDate: date,
+	}, nil
 }
